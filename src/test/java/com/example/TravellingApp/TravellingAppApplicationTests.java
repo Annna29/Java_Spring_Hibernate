f5f6a9db078c775ce4497844ab427ffa3e222d07
Ana <anna_29_10@yahoo.com>
2023-11-02 11:27:55 +0200

    first commit

---
+++ src/test/java/com/example/TravellingApp/TravellingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TravellingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
